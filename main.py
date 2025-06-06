6ae7901b4d9778298ebbb7d6e7de4770b412b90d
鄭博允 <115069485+YunYue1029@users.noreply.github.com>
2025-06-06 10:17:23 +0800

    0606_update

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ from models.tool_call import MCPRequest
 from tools import TOOL_HANDLERS
 from fastapi.responses import JSONResponse
 from fastapi import File, UploadFile
+import whisper
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ⛔ 任何來源都可呼叫（開發階段可以）
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/run")
 async def run_mcp(request: MCPRequest):
@@ -58,3 +68,16 @@ async def upload_speech_audio(file: UploadFile = File(...)):
 @app.get("/")
 def root():
     return {"message": "MCP server ready!"}
+
+@app.post("/audioToText")
+async def audio_to_text(file: UploadFile = File(...)):
+    contents = await file.read()
+    with open(f"speech_audio/{file.filename}", "wb") as f:
+        f.write(contents)
+    model = whisper.load_model("base") 
+    result = model.transcribe(f"speech_audio/{file.filename}", task="transcribe")
+
+    print(f"Transcription result: {result['text']}")
+    return {
+        "transcript": result["text"]
+    }
